da60c881936fedd7991297424b6f04d208a2cf43
ibrocods <112171866+ibrocods@users.noreply.github.com>
2025-06-25 19:43:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "./components/heroSection";
+import FaqSection from "./components/FaqSection";
+import ReviewsSection from "./components/ReviewSection";
+// app/page.tsx
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Faz Travels</h1>
-    </div>
+    <main>
+      <Hero/>
+      <ReviewsSection/>
+      <FaqSection/>
+      
+    </main>
   );
 }
